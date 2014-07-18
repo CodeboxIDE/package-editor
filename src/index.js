57449b3d93793882b5056c08fd1b9e70e8ae6b9b
Samy Pessé <samypesse@gmail.com>
2014-07-17 17:50:16 -0700

    Don't open folder
    Add unique id for tabs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 define([
     "src/tab"
 ], function(Tab) {
+    var Q = codebox.require("hr/promise");
     var commands = codebox.require("core/commands");
     var dialogs = codebox.require("utils/dialogs");
 
     var File = codebox.require("models/file");
+
+    var openFile = function(f) {
+        if (f.isDirectory()) return Q.reject(new Error("Could not open a folder"));
+
+        return codebox.tabs.add(Tab, {
+            model: f
+        }, {
+            title: f.get("name"),
+            uniqueId: "file://"+f.get("path")
+        });
+    };
 
     commands.register({
         id: "file.open",
@@ -14,13 +26,7 @@ define([
             if (!args.path) throw "Need 'path' to open a file";
 
             return File.get(args.path)
-            .then(function(f) {
-                codebox.tabs.add(Tab, {
-                    model: f
-                }, {
-                    title: f.get("name")
-                });
-            });
+            .then(openFile);
         }
     });
 });
